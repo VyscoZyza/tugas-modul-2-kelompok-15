b42f82d066ef26e7ab002fab94a8ffee0450c756
Alvin Zulham <50919691+VyscoZyza@users.noreply.github.com>
2021-09-06 20:47:37 +0700

    first component done

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Product from "./components/Product";
+import Item from "./components/Product/data";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div className="bg-white shadow">
+      <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+        <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+          {Item.map((i) => {
+            return (
+              <Product
+                id={i.id}
+                name={i.name}
+                href={i.href}
+                imageSrc={i.imageSrc}
+                imageAlt={i.imageAlt}
+                price={i.price}
+                currency={i.currency}
+                rarity={i.rarity}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </div>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
