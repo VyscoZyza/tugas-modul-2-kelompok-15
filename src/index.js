e1f3160b7a579adc5cc317ab38b510ed5271c0d3
Ahmad Naufal Athaya <49048636+AhmadNaufalA@users.noreply.github.com>
2021-09-07 21:04:35 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import Product from "./components/Product";
 import Item from "./components/Product/data";
+import Navbar from "./components/Navbar";
 
 ReactDOM.render(
   <React.StrictMode>
+    <Navbar />
     <div className="bg-white shadow">
       <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
         <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
